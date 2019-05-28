58c55a05881b6298d75218367e6ea0db591128d1
Cris Mihalache <me@f3rno.com>
2019-05-28 17:44:01 +0700

    (feature) add tests for method_generators

---
+++ test/test_util/default_db_data.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'debug_model_collection': [
+    { str: 'a', mts: 2 },
+    { str: 'b', mts: 1 },
+    { str: 'c', mts: 0 },
+  ],
+
+  'debug_model_map': {
+    'test42': { 
+      id: 'test42',
+      str: 'default test data',
+    },
+  },
+}
